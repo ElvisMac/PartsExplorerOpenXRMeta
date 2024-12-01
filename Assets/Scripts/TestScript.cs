2958361c58e067fcd0868bc4afed8d0346ed870f
Elvis McIlmoyle <elvismac@outlook.com>
2024-12-01 23:06:09 +0000

    Initial commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject item;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
